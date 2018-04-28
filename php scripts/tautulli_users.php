9d9cbefefb3998eba797103e99ad76cf057b1b2b
arejaycodes <33905011+arejaycodes@users.noreply.github.com>
2018-04-28 13:54:52 -0500

    Update tautulli_users.php

--- php scripts/tautulli_users.php
+++ php scripts/tautulli_users.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 0);
 
-$URL = "http://PLEXSERVER:PORT/api/v2?apikey=INSERTAPIKEYHERE&cmd=get_activity";
+$URL = "http://TAUTULLISERVER:PORT/api/v2?apikey=INSERTAPIKEYHERE&cmd=get_activity";
 $data = json_decode(file_get_contents($URL), true);
 
 for($c=0;$c<count($data['response']['data']['sessions']);$c++)
